dd120bf5be142424497161a6d950eb0d20d0502b
jonesjacklewis <jones.jack.lewis@gmail.com>
2022-03-25 10:11:59 +0000

    eslint

--- routes/index.js
+++ routes/index.js
@@ -1,65 +1,75 @@
-var express = require("express");
-var router = express.Router();
+const express = require('express');
+// eslint-disable-next-line new-cap
+const router = express.Router();
 
 const COLOURS = [
   {
-    name: "RED",
-    hex: "#FF0000",
+    name: 'RED',
+    hex: '#FF0000',
   },
   {
-    name: "GREEN",
-    hex: "#00FF00",
+    name: 'GREEN',
+    hex: '#00FF00',
   },
   {
-    name: "BLUE",
-    hex: "#0000FF",
+    name: 'BLUE',
+    hex: '#0000FF',
   },
   {
-    name: "BLACK",
-    hex: "#000000",
+    name: 'BLACK',
+    hex: '#000000',
   },
 ];
 
 const hexToName = {
-  "#FF0000": "RED",
-  "#00FF00": "GREEN",
-  "#0000FF": "BLUE",
-  "#000000": "BLACK",
+  '#FF0000': 'REsD',
+  '#00FF00': 'GREEN',
+  '#0000FF': 'BLUE',
+  '#000000': 'BLACK',
 };
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get('/', function(req, res, next) {
   // random hex code
   // random number 0 - 3
   let index = Math.floor(Math.random() * 4);
-  let hex = COLOURS[index]["hex"];
+  const hex = COLOURS[index]['hex'];
 
   // random text
   index = Math.floor(Math.random() * 4);
-  let name = COLOURS[index]["name"];
+  const name = COLOURS[index]['name'];
 
-  res.render("index", { title: "Stroop Effect", colour: hex, name: name });
+  res.render('index', {title: 'Stroop Effect', colour: hex, name: name});
 });
 
-router.post("/", function (req, res, next) {
+router.post('/', function(req, res, next) {
   const requestBody = req.body;
 
-  if ("selected" in requestBody) {
-    const guess = requestBody["selected"];
-    const correct = hexToName[req.cookies["correctColourGuess"]];
+  if ('selected' in requestBody) {
+    const guess = requestBody['selected'];
+    const correct = hexToName[req.cookies['correctColourGuess']];
 
     if (guess === correct) {
       res.end(
-        "<script>window.alert('Correct');window.location.href='/';</script>"
+          `<script>
+          window.alert('Correct');
+          window.location.href='/';
+          </script>`,
       );
     } else {
       res.end(
-        "<script>window.alert('Incorrect');window.location.href='/';</script>"
+          `<script>
+          window.alert('Incorrect');
+          window.location.href='/';
+          </script>`,
       );
     }
   } else {
     res.end(
-      "<script>window.alert('Invalid Request');window.location.href='/';</script>"
+        `<script>
+        window.alert('Invalid Request');
+        window.location.href='/';
+        </script>`,
     );
   }
 });
